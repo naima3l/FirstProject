3652054ed9be064a0bd77305e11ccc1d7a92c3e3
naima.el.khattabi <naima.el.khattabi@avanade.com>
2021-07-12 12:56:06 +0200

    Modified text

--- FirstProject/Program.cs
+++ FirstProject/Program.cs
@@ -7,7 +7,7 @@ namespace FirstProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Sono Naima");
+            Console.WriteLine("Sono Naima. Tutto bene?");
         }
     }
 }
